106ca39ef2e4e7b562f772202e79b9ce95cb060e
Tiago Jerônimo <tiago.batmansz@gmail.com>
2021-12-07 18:56:26 -0300

    DESAFIO 2 - Fiz uma função que calcula a área de um triângulo

--- src/challenges.js
+++ src/challenges.js
@@ -3,8 +3,8 @@ function compareTrue(b1,b2) {
   return b1 && b2; //Estava dando erro. O enuciado pede que use apenas o operador &&. Vamos vê se dá certo ¯\_(ツ)_/¯
 }
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, heigth) { //Defini dois paramentros para minha função;
+  return (base*heigth)/2 //Fiz com que a função retorna-se a multiplicação dos dois parâmetros dividido por dois;
 }
 
 // Desafio 3
